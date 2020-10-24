ae8750036c2fb4266cb2c2332e2dadbc4e25e39b
ShaunParsons <github@shaunparsons.co.uk>
2020-10-24 13:21:35 +0100

    Update install script

--- .install.sh
+++ .install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sudo apt install fonts-powerline ttf-ancient-fonts nvim
 
-# https://github.com/brymck/print-alias.git
+# Install OhMyZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
+mv bullet-train.zsh-theme ~/.oh-my-zsh/themes/bullet-train.zsh-theme
+cd .oh-my-zsh/plugins
+git clone https://github.com/brymck/print-alias
+cd ~
+
+# Install TMUX plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
